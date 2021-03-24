ea37a1bf8c4a35c546a64daa011f3df463dd90ff
makis <makisbenezis@nbg.gr>
2021-03-24 09:04:32 -0700

    ADD CODE ON BASICFUNCTINALITY AND SQLSTATEMENTS ON SQLPROPERTIES

--- src/main/java/com/acme/eshop/BasicFunctionality.java
+++ src/main/java/com/acme/eshop/BasicFunctionality.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Properties;
@@ -28,9 +29,18 @@ public class BasicFunctionality {
 		dbInstance.startH2Server();
 
 		// Create tables of the project
-		//demo.createTable();
+		demo.createTable();
 
-		// Stop H2 database server via shutdown hook
+		//BASIC FUNCTIONALITY-STEP1
+		//CREATE A LIST OF  PRODUCTS
+		demo.insertProducts();
+		//LOAD PRODUCTS IN ORDER TO BE SEEN BY THE USERS
+		demo.loadProducts();
+
+		//A NEW CUSTOMER MAKES AN ACCOUNT
+		//demo.insertNewCustomer();
+
+		// Stop H2 database server via shutdown
 		Runtime.getRuntime().addShutdownHook(new Thread(() -> dbInstance.stopH2Server()));
 
 		while (true) {
@@ -62,6 +72,41 @@ public class BasicFunctionality {
 			logger.debug("Tables created {}.", resultRows);
 		} catch (SQLException throwables) {
 			logger.error("Error creating Tables:", throwables);
+		}
+	}
+
+	private void insertProducts() {
+		try (Statement statement = ConnectionPoolProvider.getConnection().createStatement()) {
+			int resultRows = statement.executeUpdate(sqlCommands.getProperty("insert.table.001"));
+			logger.debug("Statement returned {}.", resultRows);
+			resultRows = statement.executeUpdate(sqlCommands.getProperty("insert.table.002"));
+			logger.debug("Statement returned {}.", resultRows);
+			resultRows = statement.executeUpdate(sqlCommands.getProperty("insert.table.003"));
+			logger.debug("Statement returned {}.", resultRows);
+			resultRows = statement.executeUpdate(sqlCommands.getProperty("insert.table.004"));
+			logger.debug("Statement returned {}.", resultRows);
+			resultRows = statement.executeUpdate(sqlCommands.getProperty("insert.table.005"));
+			logger.debug("Statement returned {}.", resultRows);
+			resultRows = statement.executeUpdate(sqlCommands.getProperty("insert.table.006"));
+			logger.debug("Statement returned {}.", resultRows);
+
+		} catch (SQLException throwables) {
+			logger.error("Error occurred while inserting data.", throwables);
+		}
+	}
+
+	private void loadProducts() {
+		try (Statement statement = ConnectionPoolProvider.getConnection().createStatement();
+			 ResultSet resultSet = statement.executeQuery(sqlCommands.getProperty("select.table.001"))) {
+
+			while (resultSet.next()) {
+				//@formatter:off
+				logger.info("PRODID:{}, PRODDESCR:{}, PRICE:{}", resultSet.getLong("PRODID"),
+							resultSet.getString("PRODDESCR"), resultSet.getBigDecimal("PRICE"));
+				//@formatter:on
+			}
+		} catch (SQLException throwables) {
+			logger.error("Error occurred while retrieving products", throwables);
 		}
 	}
 
